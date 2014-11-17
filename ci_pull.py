40c15362687dfdbc93fd9ecf5f38766f558196d3
mrbenji <benji.tittle@gmail.com>
2014-11-17 08:54:58 -0800

    Working standalone with media names inserted.

--- ci_pull.py
+++ ci_pull.py
@@ -2,11 +2,13 @@ import bdt_utils
 
 from openpyxl import load_workbook
 
-slog = load_workbook('11629.xlsm')
-pn_sheet = slog.get_sheet_by_name('PS1')
+eco_form = load_workbook('11629.xlsm')
+pn_sheet = eco_form.get_sheet_by_name('PS1')
 pn_rows = pn_sheet.rows
 row_num = 0
 pn_table = []
+current_media = ""
+
 for row in pn_rows:
     row_num += 1
     for cell in row:
@@ -20,16 +22,19 @@ for row in pn_rows:
         if cell.column == "B" and not pn_sheet['C'+str(cell.row)].value:
             pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
 
-        if cell.column == "C":
-            if cell.value:
-                pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
+        if cell.column == "C" and cell.value:
+            pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
 
-        if cell.column == "F":
-            if cell.value:
-                pn_table[-1][-1] = "  " * int("{:.0f}".format(cell.style.alignment.indent)) + pn_table[-1][-1]
-                pn_table[-1].append(cell.value)
+        if cell.column == "F" and cell.value:
+            pn_table[-1][-1] = "  " * int("{:.0f}".format(cell.style.alignment.indent)) + pn_table[-1][-1]
+            pn_table[-1].append(cell.value)
+
+        if cell.column == "G" and cell.value:
+            if not current_media == cell.value:
+                current_media = cell.value
+                hold_row = pn_table.pop()
+                pn_table.append(["\n\n" + current_media, ""])
+                pn_table.append(hold_row)
 
 print bdt_utils.pretty_table(pn_table)
-# for part in pn_table:
-#     print "{} Rev. {}  -  {}".format(part[0], part[1], part[2])
 
