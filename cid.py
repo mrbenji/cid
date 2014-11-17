7d245d7fb930942d69d709e4e4942eabc9bbed10
mrbenji <benji.tittle@gmail.com>
2014-11-17 14:19:09 -0800

    Reworked system to format media sets independently

--- cid.py
+++ cid.py
@@ -16,7 +16,6 @@ def extract_part_nums (filename):
     pn_sheet = eco_form.get_sheet_by_name('PS1')
     pn_rows = pn_sheet.rows
     row_num = 0
-    pn_table = []
     current_media = ""
     media_sets = {}
     skip_media = False
@@ -32,60 +31,66 @@ def extract_part_nums (filename):
                     media_sets[current_media] = []
             if current_media and not current_media in MEDIA_WE_SKIP:
                 media_sets[current_media].append(row)
+                #media_sets[current_media][-1].append(row_num)
 
-    print str(media_sets.keys())
-    row_num = 0
+    cid_tables = {}
 
-    for row in pn_rows:
-        row_num += 1
-        for cell in row:
-            if row_num < 5 or not pn_sheet['A'+str(cell.row)].value or cell.column not in "ABCFG":
-                continue
+    for set_name in media_sets.keys():
+        row_num = 0
+        cid_tables[set_name] = []
+        pn_table = cid_tables[set_name]
 
-            # "Affected Documentation" column
-            if cell.column == "A":
-                pn_table.append([])
-                pn_table[-1].append(cell.value)
+        for row in media_sets[set_name]:
+            row_num = row[0].row
+            for cell in row:
+                if not pn_sheet['A'+str(cell.row)].value or cell.column not in "ABCFG":
+                    continue
 
-            # "Cur Rev" column: we skip this if there's a value in "new rev"
-            if cell.column == "B" and not pn_sheet['C'+str(cell.row)].value:
-                pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
+                # "Affected Documentation" column
+                if cell.column == "A":
+                    pn_table.append([])
+                    pn_table[-1].append(cell.value)
 
-            # "New Rev" column
-            if cell.column == "C" and cell.value:
-                pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
+                # "Cur Rev" column: we skip this if there's a value in "new rev"
+                if cell.column == "B" and not pn_sheet['C'+str(cell.row)].value:
+                    pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
 
-            # "Description..." column
-            if cell.column == "F" and cell.value:
-                # is this row for a 139?
-                is_139 = pn_table[-1][-1][0:3] in ["139"]
+                # "New Rev" column
+                if cell.column == "C" and cell.value:
+                    pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
 
-                # if the description is indented, we need to add spaces to the part number
-                pn_table[-1][-1] = "  " * int("{:.0f}".format(cell.style.alignment.indent)) + pn_table[-1][-1]
+                # "Description..." column
+                if cell.column == "F" and cell.value:
+                    # is this row for a 139?
+                    is_139 = pn_table[-1][-1][0:3] in ["139"]
 
-                # if current PN is a 139, we want to print a blank line before it
-                if is_139:
-                    pn_table[-1][-1] = "\n" + pn_table[-1][-1]
-                pn_table[-1].append(cell.value)
+                    # if the description is indented, we need to add spaces to the part number
+                    pn_table[-1][-1] = "  " * int("{:.0f}".format(cell.style.alignment.indent)) + pn_table[-1][-1]
 
-            # "Media" column
-            if cell.column == "G":
-                if cell.value:
-                    if not current_media == cell.value:
-                        current_media = cell.value
-                        if current_media.lower() in MEDIA_WE_SKIP:
-                            skip_media = True
-                        else:
-                            skip_media = False
-                            hold_row = pn_table.pop()
-                            pn_table.append(["\n\n" + current_media, ""])
-                            pn_table.append(hold_row)
+                    # if current PN is a 139, we want to print a blank line before it
+                    if is_139:
+                        pn_table[-1][-1] = "\n" + pn_table[-1][-1]
+                    pn_table[-1].append(cell.value)
 
-                # If we're on a row for media we skip, remove entire row from results
-                if skip_media:
-                    pn_table.pop()
+                # "Media" column
+                if cell.column == "G":
+                    if cell.value:
+                        if not current_media == cell.value:
+                            current_media = cell.value
+                            if current_media.lower() in MEDIA_WE_SKIP:
+                                skip_media = True
+                            else:
+                                skip_media = False
+                                hold_row = pn_table.pop()
+                                pn_table.append(["\n\n" + current_media, ""])
+                                pn_table.append(hold_row)
 
-    return bdt_utils.pretty_table(pn_table, 4)
+                    # If we're on a row for media we skip, remove entire row from results
+                    if skip_media:
+                        pn_table.pop()
+
+    return cid_tables
+    #return bdt_utils.pretty_table(pn_table, 4)
 
 
 def print_many_cid_files(contents_id_dump):
@@ -125,21 +130,24 @@ def main():
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
 
-    contents_id_dump = extract_part_nums(arguments["eco_file"])
+    cid_dumps = extract_part_nums(arguments["eco_file"])
 
     if arguments["screen_print"]:
-        print contents_id_dump
+        for dump in cid_dumps:
+            print bdt_utils.pretty_table(cid_dumps[dump],3)
 
     if arguments["print_to_one"]:
         with open("CONTENTS_ID.all", "w") as f:
-            f.write(contents_id_dump)
+            for dump in cid_dumps:
+                f.write(bdt_utils.pretty_table(cid_dumps[dump],3))
 
     # if no flags were set, default to "print to many" -- if only -o and/or -s were set, don't print to many.
     if not arguments["screen_print"] and not arguments["print_to_one"] and not arguments["print_to_many"]:
         arguments["print_to_many"]=True
 
     if arguments["print_to_many"]:
-        print_many_cid_files(contents_id_dump)
+        for dump in cid_dumps:
+            print_many_cid_files(bdt_utils.pretty_table(cid_dumps[dump],3))
 
 if __name__ == "__main__":
     main()
