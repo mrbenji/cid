733925e3d00ddd77f508c7ac63b4cc7a4472a4ff
mrbenji <benji.tittle@gmail.com>
2014-11-17 17:43:38 -0800

    added some error checking, fixed bug with media type case mismatch

--- cid.py
+++ cid.py
@@ -6,7 +6,7 @@ import openpyxl
 HAS_NO_MEDIA = ["scif", "hard_copy", "hardcopy", "synergy"]
 
 
-def extract_part_nums (filename, all_parts=False):
+def extract_part_nums(filename, all_parts=False):
 
     if all_parts:
         media_to_skip = []
@@ -16,11 +16,16 @@ def extract_part_nums (filename, all_parts=False):
     try:
         eco_form = openpyxl.load_workbook(filename)
     except openpyxl.exceptions.InvalidFileException:
-        print '\nERROR: Could not open ECO form "{}"'.format(filename)
+        print '\nERROR: Could not open ECO form "{}"\n\n       Is path correct?'.format(filename)
         sys.exit(1)
 
     pn_sheet = eco_form.get_sheet_by_name('PS1')
-    pn_rows = pn_sheet.rows
+    try:
+        pn_rows = pn_sheet.rows
+    except AttributeError:
+        print '\nERROR: ECO form "{}" doesn\'t have a "PS1" tab.'.format(filename)
+        sys.exit(1)
+
     row_num = 0
     current_media = ""
     media_sets = {}
@@ -31,8 +36,8 @@ def extract_part_nums (filename, all_parts=False):
         row_num += 1
         if row_num > 4:
             current_media_col = pn_sheet['G'+str(row_num)].value
-            if current_media_col and not (current_media_col.strip().replace(" ", "_") == current_media):
-                current_media = str(current_media_col).strip().replace(" ", "_")
+            if current_media_col and not (current_media_col.strip().replace(" ", "_").lower() == current_media):
+                current_media = str(current_media_col).strip().replace(" ", "_").lower()
                 if not current_media in media_to_skip:
                     media_sets[current_media] = []
             if current_media and not current_media in media_to_skip:
@@ -56,26 +61,35 @@ def extract_part_nums (filename, all_parts=False):
                     pn_table[-1].append(cell.value)
 
                 # "Cur Rev" column: we skip this if there's a value in "new rev"
-                if cell.column == "B" and not pn_sheet['C'+str(cell.row)].value:
-                    pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
+                if cell.column == "B":
+                    if not cell.value:
+                        print "\nERROR on PS1 tab: P/N present in cell A{x}, but B{x} is empty.\n".format(x=cell.row)
+                        exit(1)
+                    if not pn_sheet['C'+str(cell.row)].value:
+                        pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
 
                 # "New Rev" column
                 if cell.column == "C" and cell.value:
                     pn_table[-1][-1] = pn_table[-1][-1] + " " + "Rev. " + cell.value
 
                 # "Description..." column
-                if cell.column == "F" and cell.value:
+                if cell.column == "F":
+                    if not cell.value:
+                        print "\nERROR on PS1 tab: P/N present in cell A{x}, but F{x} is empty.\n".format(x=cell.row)
+                        exit(1)
                     new_indent_level = cell.style.alignment.indent
 
                     if not current_indent_level:
                         current_indent_level = new_indent_level
 
+                    # test the current (previous row's) indent level vs. this row's indent level
                     indent_reduced = new_indent_level < current_indent_level
                     current_indent_level = new_indent_level
 
                     # if the description is indented, we need to add spaces to the part number
                     pn_table[-1][-1] = "  " * int("{:.0f}".format(current_indent_level)) + pn_table[-1][-1]
 
+                    # if the indention level was reduced, add blank line to improve legibility
                     if indent_reduced:
                         pn_table[-1][-1] = "\n" + pn_table[-1][-1]
                     pn_table[-1].append(cell.value)
@@ -109,16 +123,16 @@ def print_many_cid_files(contents_id_dump):
             stripped_line = line.strip()
             if not stripped_line[0].isdigit():
                 if current_media:
-                    OUTPUT_FILE.close()
+                    output_file.close()
                 current_media = line.strip()
                 print "Creating file CONTENTS_ID." + current_media.replace(" ", "_")
-                OUTPUT_FILE = open("CONTENTS_ID." + current_media.replace(" ", "_"), "w")
+                output_file = open("CONTENTS_ID." + current_media.replace(" ", "_"), "w")
                 continue
         if current_media:
-            OUTPUT_FILE.write(line+"\n")
+            output_file.write(line+"\n")
 
-    if not OUTPUT_FILE.closed:
-        OUTPUT_FILE.close()
+    if not output_file.closed:
+        output_file.close()
 
 
 def make_parser():
