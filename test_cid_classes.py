468686483f27f9db1ff68063200fc9d4cf11debb
mrbenji <benji.tittle@gmail.com>
2014-11-20 09:47:06 -0800

    realized I didn't need to set up all the rev objects ahead of time

--- test_cid_classes.py
+++ test_cid_classes.py
@@ -2,36 +2,29 @@ import unittest
 
 from cid_classes import *
 
-class CID_ClassesTest(unittest.TestCase):
-
-    def setUp(self):
-        self.revs = {}
-        for rev in ["-", "1", "5", "A", "B", "G", "AA", "BC", "B1", "B2", "C5", "Y"]:
-            self.revs[rev] = Rev(rev)
-        self.revs["Gdupe"] = Rev("G")
-        self.revs["5dupe"] = Rev("5")
+class CidClassesTest(unittest.TestCase):
 
     def test_alphabetic_revs(self):
-        self.assertTrue(self.revs["G"] > self.revs["A"])
-        self.assertTrue(self.revs["G"] == self.revs["Gdupe"])
-        self.assertTrue(self.revs["Y"] < self.revs["AA"])
+        self.assertTrue(Rev("G") > Rev("A"))
+        self.assertTrue(Rev("G") == Rev("G"))
+        self.assertTrue(Rev("Y") < Rev("AA"))
 
     def test_numeric_revs(self):
-        self.assertTrue(self.revs["1"] < self.revs["5"])
-        self.assertTrue(self.revs["5"] == self.revs["5dupe"])
+        self.assertTrue(Rev("1") < Rev("5"))
+        self.assertTrue(Rev("5") == Rev("5"))
 
     def test_alphanumeric_revs(self):
-        self.assertTrue(self.revs["B1"] < self.revs["B2"])
-        self.assertTrue(self.revs["C5"] > self.revs["B2"])
+        self.assertTrue(Rev("B1") < Rev("B2"))
+        self.assertTrue(Rev("C5") > Rev("B2"))
 
     def test_mixed_revs(self):
-        self.assertTrue(self.revs["B"] > self.revs["1"])
-        self.assertTrue(self.revs["1"] < self.revs["B2"])
-        self.assertTrue(self.revs["B1"] > self.revs["B"])
-        self.assertTrue(self.revs["B"] < self.revs["B2"])
-        self.assertTrue(self.revs["-"] < self.revs["1"])
-        self.assertTrue(self.revs["-"] < self.revs["A"])
-        self.assertTrue(self.revs["C5"] > self.revs["-"])
+        self.assertTrue(Rev("B") > Rev("1"))
+        self.assertTrue(Rev("1") < Rev("B2"))
+        self.assertTrue(Rev("B1") > Rev("B"))
+        self.assertTrue(Rev("B") < Rev("B2"))
+        self.assertTrue(Rev("-") < Rev("1"))
+        self.assertTrue(Rev("-") < Rev("A"))
+        self.assertTrue(Rev("C5") > Rev("-"))
 
     def test_invalid_revs(self):
         for bad_rev in ["BO", "I", "AZ", "11", "B21", "1B1", "-A"]:
