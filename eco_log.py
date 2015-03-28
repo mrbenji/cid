f885cb98b1015e6458f58fd82c08ae49e318b237
mrbenji <benji.tittle@gmail.com>
2015-03-27 21:07:46 -0700

    getting eco log read closer to ready

--- eco_log.py
+++ eco_log.py
@@ -7,14 +7,13 @@ import sys
 from collections import defaultdict
 
 # Third Party Open Source Libs
-from xlwings import Workbook, Sheet, Range, RowCol   # Control Excel via COM. https://pypi.python.org/pypi/xlwings/0.3.4
-import openpyxl
-import six                                   # https://pypi.python.org/pypi/six/1.9.0
+from xlwings import Workbook, Sheet, Range  # Control Excel via COM. https://pypi.python.org/pypi/xlwings/0.3.4
+import openpyxl                             # https://pypi.python.org/pypi/openpyxl/2.2.0
 
 import cid
 
 
-def calc_first_blank():
+def calc_first_blank_xlwings():
     return len(Range('A1').vertical) + 1
 
 
@@ -23,7 +22,7 @@ def error_text(error):
     return textwrap.fill(exc[2])
 
 
-def save_workbook(workbook, filename):
+def save_workbook_xlwings(workbook, filename):
     try:
         workbook.save(filename)
     except pywintypes.com_error as error:
@@ -33,7 +32,7 @@ def save_workbook(workbook, filename):
     return True
 
 
-def open_workbook(workbook_path):
+def open_workbook_xlwings(workbook_path):
 
     try:
         wb = Workbook(workbook_path)
@@ -44,7 +43,7 @@ def open_workbook(workbook_path):
     return wb
 
 
-def activate_sheet(sheet_name):
+def activate_sheet_xlwings(sheet_name):
     try:
         Sheet(sheet_name).activate()
     except pywintypes.com_error as error:
@@ -52,7 +51,7 @@ def activate_sheet(sheet_name):
         return None
 
 
-def read_workbook(wb_path, sheet_name, sheet_id):
+def read_openpyxl(wb_path, sheet_name, sheet_id):
 
     try:
         # openpyxl is a library for reading/writing Excel files.
@@ -70,29 +69,40 @@ def read_workbook(wb_path, sheet_name, sheet_id):
                     '\n\n     {}'.format(wb_path))
         sys.exit()
 
-    wb_data = defaultdict()
+    eco_data = defaultdict()
+
+    wb_update_table = []
 
     row_num = 1
 
+    # marked_unused = []
+    marked_unused = ["cancelled", "unused", "void", "reuse me!"]
+
     for row in wb_rows:
         if row_num < 4:
+            row_num += 1
             continue
 
-        if row[1].value:
-            eco_num = str(row[0].value)
-            wb_data[eco_num] = defaultdict()
-            wb_data[eco_num]["date_assigned"] = row[1].value
-            wb_data[eco_num]["initiator"] = row[2].value
-            wb_data[eco_num]["part_number"] = row[3].value
-            wb_data[eco_num]["project_data"] = row[4].value
+        if row[1].value and str(row[1].value).lower() not in marked_unused:
+            eco_num = row[0].value
+            print(eco_num)
+            eco_data[eco_num] = defaultdict()
+            eco_data[eco_num]["date_assigned"] = row[1].value
+            eco_data[eco_num]["initiator"] = row[2].value
+            eco_data[eco_num]["part_number"] = row[3].value
+            eco_data[eco_num]["project_data"] = row[4].value
+            eco_data[eco_num]["incorp_date"] = row[5].value
+            wb_update_table.append([eco_num, row[1].value, row[2].value, row[3].value, row[4].value, row[5].value])
 
         row_num += 1
 
-    return 1
+    return eco_data
 
 
 def main():
-    read_workbook(r"c:\cid-tool\cid\ECO.xlsx", "ALL ECO's", "ECO Log")
+    eco_data = read_openpyxl(r"c:\cid-tool\cid\ECO.xlsx", "ALL ECO's", "ECO Log")
+    if eco_data:
+        print("Completed.")
 
 if __name__ == "__main__":
     main()
